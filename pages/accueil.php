b82398ce118df01a7fcd5414073003a562cd734a
tiago <tiago.dasilvamendes@condorcet.be>
2022-02-23 13:36:50 +0100

    indexer le fichier

---
+++ pages/accueil.php
@@ -0,0 +1,14 @@
+<?php
+$query = 'select * from p_ville';
+$result = $cnx-> query($query);
+
+$data = $result->fetchAll();
+//var_dump($data);
+/*foreach($data as $d){
+    print '<br>'.$d[]
+}*/
+
+$nbr=count($data);
+for($i=0;$i<$nbr;$i++){
+    print "<br>".$data[$i]['nom_ville']."(".$data[$i]['code_postal'].")";
+}
